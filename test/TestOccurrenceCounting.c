7c120eab46e95f5464b75e7ee5e522a7336baa1d
Cxx-mlr <spapiernik12@gmail.com>
2024-10-19 20:33:36 -0500

    Implement unit tests for occurrence counting functions

--- test/TestOccurrenceCounting.c
+++ test/TestOccurrenceCounting.c
@@ -1,2 +1,9 @@
 #include "unity.h"
 #include "integer_toolkit.h"
+
+void test_count_occurrences_of() {
+  TEST_ASSERT_EQUAL(2, intk_count_occurrences_of(133, 3));
+  TEST_ASSERT_EQUAL(1, intk_count_occurrences_of(133, 33));
+  TEST_ASSERT_EQUAL(3, intk_count_occurrences_of(71191, 1));
+  TEST_ASSERT_EQUAL(0, intk_count_occurrences_of(71191, 8));
+}
