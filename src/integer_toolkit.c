ca06b456f4906fc8f50c7c60304d31d0631a1a07
Cxx-mlr <spapiernik12@gmail.com>
2024-10-19 20:41:50 -0500

    Add intk_ prefix to all functions

--- src/integer_toolkit.c
+++ src/integer_toolkit.c
@@ -3,102 +3,105 @@
 #include <assert.h>
 #include <stdarg.h>
 
+#include <stdio.h>
+
 #include "integer_toolkit.h"
 
 // Integer Properties and Validation Functions
 
-unsigned int length(int number) {
+unsigned int intk_length(int number) {
     if (number == 0) return 1;
     return (unsigned int) floor(log10(abs(number))) + 1;
 }
 
-int is_empty(int number) {
+int intk_is_empty(int number) {
     if (number == 0) {
         return 1;
     }
     return 0;
 }
 
-int in_range(int number, int start, int stop) {
+int intk_in_range(int number, int start, int stop) {
     return number >= start && number < stop;
 }
 
-int index_ok(int number, int index) {
-    return in_range(index, 0, length(number));
+int intk_index_ok(int number, int index) {
+    return intk_in_range(index, 0, intk_length(number));
 }
 
 // Integer Manipulation Functions
 
-void push_front(int *number, int digit_or_number) {
-    if (!is_empty(*number)) {
-        *number += with_zeros(digit_or_number, length(*number));
+void intk_push_front(int *number, int digit_or_number) {
+    if (!intk_is_empty(*number)) {
+        *number += intk_with_zeros(digit_or_number, intk_length(*number));
     }
     else {
         *number = digit_or_number;
     }
 }
 
-void push_back(int *number, int digit_or_number) {
-    *number = with_zeros(*number, length(digit_or_number)) + digit_or_number;
+void intk_push_back(int *number, int digit_or_number) {
+    *number = intk_with_zeros(*number, intk_length(digit_or_number)) + digit_or_number;
 }
 
-int pop_front_n(int *number, int n) {
-    int popped_value = front_n(*number, n);
-    *number = back_n(*number, (int) fdim(length(*number), n));
+int intk_pop_front_n(int *number, int n) {
+    int popped_value = intk_front_n(*number, n);
+    *number = intk_back_n(*number, (int) fdim(intk_length(*number), n));
     return popped_value;
 }
 
-int pop_back_n(int *number, int n) {
-    int popped_value = back_n(*number, n);
-    *number = front_n(*number, (int) fdim(length(*number), n));
+int intk_pop_back_n(int *number, int n) {
+    int popped_value = intk_back_n(*number, n);
+    *number = intk_front_n(*number, (int) fdim(intk_length(*number), n));
     return popped_value;
 }
 
-int pop_front(int *number) {
-    return pop_front_n(number, 1);
+int intk_pop_front(int *number) {
+    return intk_pop_front_n(number, 1);
 }
 
-int pop_back(int *number) {
-    return pop_back_n(number, 1);
+int intk_pop_back(int *number) {
+    return intk_pop_back_n(number, 1);
 }
 
-void insert(int *number, int index, int digit_or_number) {
-    unsigned int number_length = length(*number);
-    assert(in_range(index, 0, number_length + 1));
-    int popped_value = pop_back_n(number, number_length - index);
+void intk_insert(int *number, int index, int digit_or_number) {
+    index = intk_clamp(index, 0, intk_length(*number));
 
-    push_back(number, digit_or_number);
-    if (!is_empty(popped_value)) {
-        push_back(number, popped_value);
+    unsigned int number_length = intk_length(*number);
+    int popped_value = intk_pop_back_n(number, number_length - index);
+
+    intk_push_back(number, digit_or_number);
+    if (!intk_is_empty(popped_value)) {
+        intk_push_back(number, popped_value);
     }
 }
 
 // Integer Inspection Functions
 
-int front_n(int number, int n) {
-    return (int) floor(number / pow(10, fdim(length(number), n)));
+int intk_front_n(int number, int n) {
+    return (int) floor(number / pow(10, fdim(intk_length(number), n)));
 }
 
-int back_n(int number, int n) {
+int intk_back_n(int number, int n) {
     return number % (int) pow(10, n);
 }
 
-int front(int number) {
-    return front_n(number, 1);
+int intk_front(int number) {
+    return intk_front_n(number, 1);
 }
 
-int back(int number) {
-    return back_n(number, 1);
+int intk_back(int number) {
+    return intk_back_n(number, 1);
 }
 
-int at(int number, int index) {
-    assert(index_ok(number, index));
-    return back(front_n(number, index + 1));
+int intk_at(int number, int index) {
+    assert(intk_index_ok(number, index));
+    return intk_back(intk_front_n(number, index + 1));
 }
 
 // Utility Functions
 
-int concat(int argc, ...){
+int intk_concat(int argc, ...){
     va_list argv;
     va_start(argv, argc);
 
@@ -108,19 +111,19 @@ int concat(int argc, ...){
         int number = va_arg(argv, int);
         if (number < 0) continue;
 
-        push_back(&result, number);
+        intk_push_back(&result, number);
     }
 
     va_end(argv);
     return result;
 }
 
-int with_zeros(int number, int quantity) {
+int intk_with_zeros(int number, int quantity) {
     assert(quantity >= 0);
     return number * (int) floor(pow(10, quantity));
 }
 
-void set_digit(int *number, int index, int digit_or_number) {
+void intk_set_digit(int *number, int index, int digit_or_number) {
     assert(index_ok(*number, index));
     int number_length = length(*number);
     int bn = back_n(*number, fdim(length(*number), index + 1));
@@ -132,57 +135,67 @@ void set_digit(int *number, int index, int digit_or_number) {
     *number = with_zeros(*number, (int) fdim(number_length, length(*number)));
 }
 
-void swap(int *number, int i, int j) {
-    assert(index_ok(*number, i));
-    assert(index_ok(*number, j));
+void intk_swap(int *number, int i, int j) {
+    assert(intk_index_ok(*number, i));
+    assert(intk_index_ok(*number, j));
 
-    int tmp_i = at(*number, i);
+    int tmp_i = intk_at(*number, i);
 
-    set_digit(number, i, at(*number, j));
-    set_digit(number, j, tmp_i);
+    intk_set_digit(number, i, intk_at(*number, j));
+    intk_set_digit(number, j, tmp_i);
+}
+
+int intk_clamp(int number, int min, int max) {
+    if (number < min) {
+        return min;
+    }
+    else if (number > max) {
+        return max;
+    }
+    return number;
 }
 
 // String-Like Functions
 
-int contains(int number, int digit_or_number) {
-    return find_first_of(number, digit_or_number) != -1;
+int intk_contains(int number, int digit_or_number) {
+    return intk_find_first_of(number, digit_or_number) != -1;
 }
 
-int startswith(int number, int digit_or_number) {
-    return front_n(number, length(digit_or_number)) == digit_or_number;
+int intk_startswith(int number, int digit_or_number) {
+    return intk_front_n(number, intk_length(digit_or_number)) == digit_or_number;
 }
 
-int endswith(int number, int digit_or_number) {
-    return back_n(number, length(digit_or_number)) == digit_or_number;
+int intk_endswith(int number, int digit_or_number) {
+    return intk_back_n(number, intk_length(digit_or_number)) == digit_or_number;
 }
 
-int reversed(int number) {
+int intk_reversed(int number) {
     int result = 0;
-    while (!is_empty(number)) {
-        push_back(&result, pop_back(&number));
+    while (!intk_is_empty(number)) {
+        intk_push_back(&result, intk_pop_back(&number));
     }
     return result;
 }
 
 // Search Functions
 
-int find_first_of(int number, int digit_or_number) {
-    int number_length = length(number);
-    int digit_or_number_length = length(digit_or_number);
+int intk_find_first_of(int number, int digit_or_number) {
+    int number_length = intk_length(number);
+    int digit_or_number_length = intk_length(digit_or_number);
     for (int from = 0; from + digit_or_number_length <= number_length; from += 1) {
-        if (number_from_to(number, from, from + digit_or_number_length) == digit_or_number) {
+        if (intk_number_from_to(number, from, from + digit_or_number_length) == digit_or_number) {
             return from;
         }
     }
     return -1;
 }
 
-int find_last_of(int number, int digit_or_number) {
-    int number_length = length(number);
-    int digit_or_number_length = length(digit_or_number);
+int intk_find_last_of(int number, int digit_or_number) {
+    int number_length = intk_length(number);
+    int digit_or_number_length = intk_length(digit_or_number);
     
     for (int to = number_length; to - digit_or_number_length >= 0; to -= 1) {
-        if (number_from_to(number, to - digit_or_number_length, to) == digit_or_number) {
+        if (intk_number_from_to(number, to - digit_or_number_length, to) == digit_or_number) {
             return to - digit_or_number_length;
         }
     }
@@ -191,54 +204,56 @@ int find_last_of(int number, int digit_or_number) {
 
 // Remove Functions
 
-void remove_first_of(int *number, int digit_or_number) {
-    int index = find_first_of(*number, digit_or_number);
+void intk_remove_first_of(int *number, int digit_or_number) {
+    int index = intk_find_first_of(*number, digit_or_number);
     assert(index != -1);
 
-    int fn = number_from_to(*number, 0, index);
-    int bn = number_from_to(*number, index + length(digit_or_number), length(*number));
-    if (is_empty(bn)) {
+    int fn = intk_number_from_to(*number, 0, index);
+    int bn = intk_number_from_to(*number, index + intk_length(digit_or_number), intk_length(*number));
+    if (intk_is_empty(bn)) {
         bn = -1;
     }
-    *number = concat(2, fn, bn);
+    *number = intk_concat(2, fn, bn);
 }
 
-void remove_last_of(int *number, int digit_or_number) {
-    int index = find_last_of(*number, digit_or_number);
+void intk_remove_last_of(int *number, int digit_or_number) {
+    int index = intk_find_last_of(*number, digit_or_number);
     assert(index != -1);
 
-    int fn = number_from_to(*number, 0, index);
-    int bn = number_from_to(*number, index + length(digit_or_number), length(*number));
-    if (is_empty(bn)) {
+    int fn = intk_number_from_to(*number, 0, index);
+    int bn = intk_number_from_to(*number, index + intk_length(digit_or_number), intk_length(*number));
+    if (intk_is_empty(bn)) {
         bn = -1;
     }
-    *number = concat(2, fn, bn);
+    *number = intk_concat(2, fn, bn);
 }
 
-void remove_all(int *number, int digit_or_number) {
-    while (contains(*number, digit_or_number)) {
-        remove_first_of(number, digit_or_number);
+void intk_remove_all(int *number, int digit_or_number) {
+    while (intk_contains(*number, digit_or_number)) {
+        intk_remove_first_of(number, digit_or_number);
     }
 }
 
-int remove_digit_if(int number, int(*predicate)(int)) {
+void intk_remove_digit_if(int *number, int(*predicate)(int)) {
     int result = 0;
-    while (!is_empty(number)) {
-        int popped_value = pop_back(&number);
+    while (!intk_is_empty(*number)) {
+        int popped_value = intk_pop_back(number);
         if (predicate(popped_value)) continue;
         else {
-            push_front(&result, popped_value);
+            intk_push_front(&result, popped_value);
         }
     }
-    return result;
+    *number = result;
 }
 
-int count_occurrences_of(int number, int digit_or_number) {
-    int number_length = length(number);
-    int digit_or_number_length = length(digit_or_number);
+// Count Functions
+
+int intk_count_occurrences_of(int number, int digit_or_number) {
+    int number_length = intk_length(number);
+    int digit_or_number_length = intk_length(digit_or_number);
     int counter = 0;
     for (int from = 0; from + digit_or_number_length <= number_length; ++from) {
-        if (number_from_to(number, from, from + digit_or_number_length) == digit_or_number) {
+        if (intk_number_from_to(number, from, from + digit_or_number_length) == digit_or_number) {
             counter += 1;
         }
     }
@@ -247,11 +262,11 @@ int count_occurrences_of(int number, int digit_or_number) {
 
 // Min Max Functions
 
-int max_digit(int number) {
-    int number_length = length(number);
+int intk_max_digit(int number) {
+    int number_length = intk_length(number);
     int max_digit = 0;
     for (int index = 0; index < number_length; ++index) {
-        int current_digit = at(number, index);
+        int current_digit = intk_at(number, index);
         if (current_digit > max_digit) {
             max_digit = current_digit;
         }
@@ -259,11 +274,11 @@ int max_digit(int number) {
     return max_digit;
 }
 
-int min_digit(int number) {
-    int number_length = length(number);
+int intk_min_digit(int number) {
+    int number_length = intk_length(number);
     int min_digit = 9;
     for (int index = 0; index < number_length; ++index) {
-        int current_digit = at(number, index);
+        int current_digit = intk_at(number, index);
         if (current_digit < min_digit) {
             min_digit = current_digit;
         }
@@ -273,62 +288,62 @@ int min_digit(int number) {
 
 // Reduction Functions
 
-int reduce(int number, int(*func)(int, int), int initial) {
-    while (!is_empty(number)) {
-        unsigned int number_length = length(number);
-        int popped_value = pop_front(&number);
+int intk_reduce(int number, int(*func)(int, int), int initial) {
+    while (!intk_is_empty(number)) {
+        unsigned int number_length = intk_length(number);
+        int popped_value = intk_pop_front(&number);
         initial = func(initial, popped_value);
-        if (length(number) < number_length - 1) {
+        if (intk_length(number) < number_length - 1) {
             initial = func(initial, 0);
         }
     }
     return initial;
 }
 
-int sum_digits(int number) {
+int intk_sum_digits(int number) {
     int result = 0;
-    while (!is_empty(number)) {
-        result += pop_back(&number);
+    while (!intk_is_empty(number)) {
+        result += intk_pop_back(&number);
     }
     return result;
 }
 
 // Range Functions
 
-int range(int start, int stop, int step) {
+int intk_range(int start, int stop, int step) {
     assert(start >= 0);
     assert(step > 0);
     assert(start <= stop);
 
     int result = 0;
     while (start < stop) {
-        push_back(&result, start);
+        intk_push_back(&result, start);
         start += step;
     }
 
     return result;
 }
 
-int number_from_to(int number, int from, int to) {
-    int number_length = length(number);
-    assert(in_range(from, 0, number_length + 1));
-    assert(in_range(to, 0, number_length + 1));
+int intk_number_from_to(int number, int from, int to) {
+    int number_length = intk_length(number);
+    assert(intk_in_range(from, 0, number_length + 1));
+    assert(intk_in_range(to, 0, number_length + 1));
     assert(from <= to);
 
-    (void) pop_back_n(&number, length(number) - to);
-    return back_n(number, (int) abs(to - from));
+    (void) intk_pop_back_n(&number, intk_length(number) - to);
+    return intk_back_n(number, (int) abs(to - from));
 }
 
 // Sorting Functions
 
-void bubble_sort(int *number) {
-    remove_all(number, 0);
+void intk_bubble_sort(int *number) {
+    intk_remove_all(number, 0);
 
-    int iterations = length(*number);
+    int iterations = intk_length(*number);
     for (int i = 0; i < iterations - 1; ++i) {
         for (int j = 0; j < iterations - 1; ++j) {
-            if (at(*number, j) > at(*number, j + 1)) {
-                swap(number, j, j + 1);
+            if (intk_at(*number, j) > intk_at(*number, j + 1)) {
+                intk_swap(number, j, j + 1);
             }
         }
     }
@@ -336,29 +351,29 @@ void bubble_sort(int *number) {
 
 // Divisibility Functions
 
-int divisible_by_2(int number) {
-    if (back(number) % 2 == 0) {
+int intk_divisible_by_2(int number) {
+    if (intk_back(number) % 2 == 0) {
         return 1;
     }
     return 0;
 }
 
-int divisible_by_3(int number) {
-    if (sum_digits(number) % 3 == 0) {
+int intk_divisible_by_3(int number) {
+    if (intk_sum_digits(number) % 3 == 0) {
         return 1;
     }
     return 0;
 }
 
-int divisible_by_4(int number) {
-    if (back_n(number, 2) % 4 == 0) {
+int intk_divisible_by_4(int number) {
+    if (intk_back_n(number, 2) % 4 == 0) {
         return 1;
     }
     return 0;
 }
 
-int divisible_by_5(int number) {
-    if (back(number) % 5 == 0) {
+int intk_divisible_by_5(int number) {
+    if (intk_back(number) % 5 == 0) {
         return 1;
     }
     return 0;
