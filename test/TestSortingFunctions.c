c243bc2664dc330f8842c515f04b70056d389067
Cxx-mlr <spapiernik12@gmail.com>
2024-10-19 20:35:30 -0500

    Implement unit tests for sorting functions

--- test/TestSortingFunctions.c
+++ test/TestSortingFunctions.c
@@ -1,2 +1,13 @@
 #include "unity.h"
 #include "integer_toolkit.h"
+
+void test_bubble_sort() {
+  int number;
+
+  number = 15479;
+  intk_bubble_sort(&number);
+  TEST_ASSERT_EQUAL(14579, number);
+  number = 1001;
+  intk_bubble_sort(&number);
+  TEST_ASSERT_EQUAL(11, number);
+}
