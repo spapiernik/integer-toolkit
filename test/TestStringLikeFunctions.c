3fd3dddbca0c1d467d82e703f5739db4161cc018
Cxx-mlr <spapiernik12@gmail.com>
2024-10-19 20:35:54 -0500

    Implement unit tests for string-like functions

--- test/TestStringLikeFunctions.c
+++ test/TestStringLikeFunctions.c
@@ -1,2 +1,22 @@
 #include "unity.h"
 #include "integer_toolkit.h"
+
+void test_contains() {
+  TEST_ASSERT_TRUE(intk_contains(34120, 1));
+  TEST_ASSERT_FALSE(intk_contains(34120, 9));
+}
+
+void test_startswith() {
+  TEST_ASSERT_TRUE(intk_startswith(34120, 3));
+  TEST_ASSERT_FALSE(intk_startswith(34120, 4));
+}
+
+void test_endswith() {
+  TEST_ASSERT_TRUE(intk_endswith(34120, 0));
+  TEST_ASSERT_FALSE(intk_endswith(34120, 2));
+}
+
+void test_reversed() {
+  TEST_ASSERT_EQUAL(235711, intk_reversed(117532));
+  TEST_ASSERT_EQUAL(1985, intk_reversed(5891));
+}
